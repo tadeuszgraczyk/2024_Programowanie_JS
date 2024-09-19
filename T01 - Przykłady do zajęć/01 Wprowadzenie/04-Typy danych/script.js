9cd4be8817cd29a05e82b4e3285d1a6319f18cc4
Laboratorium <laboratorium@zszglogow.pl>
2024-09-19 23:42:05 +0200

    Rozbudowa typów i liczby pseudolosowe

--- T01 - Przykłady do zajęć/01 Wprowadzenie/04-Typy danych/script.js
+++ T01 - Przykłady do zajęć/01 Wprowadzenie/04-Typy danych/script.js
@@ -5,6 +5,25 @@ const firstName = 'Sara';
 const age = 30;
 const temp = 98.9;
 
+let intNr = 1;
+let decNr = 1.5;
+let expNr = 1.4e15;
+let octNr = 0o10; // Wartość dziesiętna to 8.
+let hexNr = 0x3E8; // Wartość dziesiętna to 1000.
+let binNr = 0b101; // Wartość dziesiętna to 5.
+console.log(octNr)
+console.log(hexNr)
+console.log(binNr)
+
+// BigInt
+let bigNr = 90071992547409920n;
+console.log(bigNr)
+let result = bigNr + intNr;
+/************************************
+O nie, otrzymaliśmy błąd typu TypeError. Z wyświetlonego komunikatu błędu jasno wynika,
+    jaka jest jego przyczyna: podczas przeprowadzania operacji nie można łączyć typów BigInt
+i Number. O tym trzeba pamiętać podczas pracy z wartościami BigInt — współdziałają one jedynie z innymi wartościami typu BigInt.
+************************************/
 // Boolean
 const hasKids = true;
 
@@ -42,3 +61,4 @@ console.log(output, typeof output);
 
 //  Więcej informacji o typie „obiekt funkcji”
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
