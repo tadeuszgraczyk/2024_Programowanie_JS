51ee8528cfc07b6328ac2b04cb12ef5301352273
Laboratorium <laboratorium@zszglogow.pl>
2024-09-22 18:03:39 +0200

    Temat 12

--- T01 - Przykłady do zajęć/01 Wprowadzenie/12-math-object/script.js
+++ T01 - Przykłady do zajęć/01 Wprowadzenie/12-math-object/script.js
@@ -1,33 +1,33 @@
 let x;
 
-// Square root
+// Pierwiastek kwadratowy
 x = Math.sqrt(9);
 
-// Absolute value
+// Wartość bezwzględna
 x = Math.abs(-5);
 
-// Round
+// Zaokrąglenie
 x = Math.round(4.2);
 
-// Round up
+// Zaokrąglenie w górę
 x = Math.ceil(4.2);
 
-// Round down
+// Zaokrąglenie w dół
 x = Math.floor(4.9);
 
-// Exponent
+// Pierwiastek
 x = Math.pow(2, 3);
 
-// Minimum number
+// Minimum
 x = Math.min(4, 5, 3);
 
-// Maximum number
+// Maksimum
 x = Math.max(4, 5, 3);
 
-// Get a random number/decimal between 0 and 1
+// Liczba pseudolosowa między 0 i 1
 x = Math.random();
 
-// Get a random number between 1 and 100
+// Liczba losowa od 1 do 100
 x = Math.floor(Math.random() * 100 + 1);
 
 console.log(x);
