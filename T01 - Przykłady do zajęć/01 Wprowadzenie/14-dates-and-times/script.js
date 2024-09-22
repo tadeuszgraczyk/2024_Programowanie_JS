88b5031b825890faefc47317c88ca73458f1e339
Laboratorium <laboratorium@zszglogow.pl>
2024-09-22 18:45:04 +0200

    Temat 14

--- T01 - Przykłady do zajęć/01 Wprowadzenie/14-dates-and-times/script.js
+++ T01 - Przykłady do zajęć/01 Wprowadzenie/14-dates-and-times/script.js
@@ -1,16 +1,16 @@
 let d;
 
-// Get today's date and time
+// Pobierz dzisiejszą datę i godzinę
 d = new Date();
 
-// Set to a string
+// Ustawienie na string
 d = d.toString();
 
-// Get a specific date
-// Important: the month is 0-based, so 0 is January and 11 is December
+// Uzyskaj konkretną datę
+// Ważne: miesiąc jest oparty na 0, więc 0 to styczeń, a 11 to grudzień
 d = new Date(2021, 0, 10, 12, 30, 0);
 
-// Pass in a string
+// Przekaż ciąg
 d = new Date('2021-07-10T12:30:00');
 d = new Date('07/10/2021 12:30:00');
 d = new Date('2022-07-10');
@@ -18,18 +18,18 @@ d = new Date('07-10-2022');
 
 // https://stackoverflow.com/questions/7556591/is-the-javascript-date-object-always-one-day-off
 
-// Get current timestamp
+// Pobierz aktualny znacznik czasu
 d = Date.now();
 
-// Get the timestamp of a specific date
+// Pobierz znacznik czasu określonej daty
 d = new Date();
 d = d.getTime();
 d = d.valueOf();
 
-// Create a date from a timestamp
+// Utwórz datę ze znacznika czasu
 d = new Date(1666962049745);
 
-// Convert from milliseconds to seconds
+// Konwersja z milisekund na sekundy
 d = Math.floor(Date.now() / 1000);
 
 console.log(d);
