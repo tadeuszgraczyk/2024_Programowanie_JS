5cd3c7e90af7827c9512be07602fe8d50e118422
Uczen 1 <teraz@wp.pl>
2024-10-09 13:53:34 +0200

    Zmiana let dla przykładów 24, 25, 27,28

--- T00 - Przykłady z podręcznika/Przykład 3_28 - continue/script.js
+++ T00 - Przykłady z podręcznika/Przykład 3_28 - continue/script.js
@@ -1,6 +1,6 @@
 //Przykład 3.28
-for (var i = 0; i <= 30; i++){
-    if ((i % 3) != 0){
+for (let i = 0; i <= 30; i++){
+    if ((i % 3) !== 0){
         continue;
     }
     document.write(i + "; ");
